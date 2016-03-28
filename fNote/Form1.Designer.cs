1acdd3225863af61f0e68d036d1f4762050a7c47
Alexey Kuzmin <alexey.kuzmin.7@mail.ru>
2016-03-29 00:56:13 +0800

    Save changes mecanism, bug fixing

--- fNote/Form1.Designer.cs
+++ fNote/Form1.Designer.cs
@@ -33,6 +33,8 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // baseSelect
@@ -56,11 +58,12 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Location = new System.Drawing.Point(12, 41);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(298, 346);
+            this.textBox1.Size = new System.Drawing.Size(298, 317);
             this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // listBox1
             // 
@@ -69,21 +72,42 @@
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(267, 342);
             this.listBox1.TabIndex = 4;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(317, 12);
+            this.button1.Location = new System.Drawing.Point(209, 10);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(33, 23);
+            this.button1.Size = new System.Drawing.Size(60, 23);
             this.button1.TabIndex = 5;
             this.button1.Text = "OK";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(55, 13);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(148, 20);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // mainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(791, 370);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.textBox1);
@@ -104,6 +128,8 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- fNote/Form1.cs
+++ fNote/Form1.cs
@@ -17,6 +17,12 @@ namespace fNote
     {
 
         string baseName = "mybase";
+        DataSet ds;
+        SQLiteConnection m_dbConnection;
+        bool textDirty = false;
+        long lastId = -1;
+        bool doingRefresh = false;
+        bool click1 = false;
         public mainForm()
         {
             InitializeComponent();
@@ -36,37 +42,103 @@ namespace fNote
             try
             {
                 string filename = "E:\\_Programing\\Projects\\fNote\\fNote\\bin\\Debug\\bases\\" + baseName + ".db";
-                if (!System.IO.File.Exists(filename))
-                {
-                    SQLiteConnection m_dbConnection2;
-                    m_dbConnection2 = new SQLiteConnection("Data Source=" + filename + "; Version=3;");
-                    m_dbConnection2.Open();
-                    string sql3 = "CREATE TABLE notes (id int, name varchar(30), note varchar(1000))";
-                    SQLiteCommand command3 = new SQLiteCommand(sql3, m_dbConnection2);
-                    command3.ExecuteNonQuery();
-
-                    string sql4 = "INSERT INTO notes (id, name, note) VALUES (1, 'a', 'abc')";
-                    SQLiteCommand command4 = new SQLiteCommand(sql4, m_dbConnection2);
-                    command4.ExecuteNonQuery();
-                }
-                else
-                {
-                    MessageBox.Show("База с таким названием уже ссуществует");
-                }
-                SQLiteConnection m_dbConnection;
-                m_dbConnection = new SQLiteConnection("Data Source="+filename+"; Version=3;");
+                bool databaseExists = System.IO.File.Exists(filename);
+                m_dbConnection = new SQLiteConnection("Data Source=" + filename + "; Version=3;");
                 m_dbConnection.Open();
 
-                DataSet ds = new DataSet();
-                SQLiteDataAdapter adapter = new SQLiteDataAdapter("SELECT name FROM notes", m_dbConnection);
-                adapter.Fill(ds);
-                this.listBox1.DataSource = ds.Tables[0];
-                this.listBox1.DisplayMember = "name";
+                if (!databaseExists)
+                {
+                    string sql3 = "CREATE TABLE notes (id integer primary key, name varchar(30), note varchar(1000))";
+                    SQLiteCommand command3 = new SQLiteCommand(sql3, m_dbConnection);
+                    command3.ExecuteNonQuery();
+
+                    string sql4 = "INSERT INTO notes (id, name, note) VALUES (NULL, 'a', 'abc'), (NULL, 'b', 'abcd')";
+                    SQLiteCommand command4 = new SQLiteCommand(sql4, m_dbConnection);
+                    command4.ExecuteNonQuery();
+                }
+
+                refresh();
+
             }
             catch (Exception ex)
             {
                 MessageBox.Show("Error" + ex);
             }
+        }
+
+        private void refresh()
+        {
+            doingRefresh = true;
+            ds = new DataSet();
+            SQLiteDataAdapter adapter = new SQLiteDataAdapter("SELECT * FROM notes", m_dbConnection);
+            adapter.Fill(ds);
+            this.listBox1.DataSource = ds.Tables[0];
+            this.listBox1.DisplayMember = "name";
+            doingRefresh = false;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (doingRefresh)
+                return;
+            try
+            {
+                if (textDirty && click1 == false)
+                {
+                    DialogResult dialogResult = MessageBox.Show("Сохранить изменения?", "", MessageBoxButtons.YesNo);
+                    if (dialogResult == DialogResult.Yes)
+                    {
+                        SQLiteCommand command = new SQLiteCommand();
+                        command.CommandText = "UPDATE notes SET name='" + textBox2.Text + "', note='" + textBox1.Text + "' WHERE id=" + lastId;
+                        command.Connection = m_dbConnection;
+                        command.ExecuteNonQuery();
+
+                        refresh();     
+
+                        MessageBox.Show("Сохранено");
+                    }
+                }
+            
+                textBox1.Text = ds.Tables[0].Rows[listBox1.SelectedIndex].Field<string>("note");
+                textBox2.Text = ds.Tables[0].Rows[listBox1.SelectedIndex].Field<string>("name");
+                label1.Text = ds.Tables[0].Rows[listBox1.SelectedIndex].Field<long>("id").ToString();
+                click1 = false;
+                textDirty = false;
+                lastId = ds.Tables[0].Rows[listBox1.SelectedIndex].Field<long>("id");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error" + ex);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SQLiteCommand command = new SQLiteCommand();
+                command.CommandText = "UPDATE notes SET name='" + textBox2.Text + "', note='" + textBox1.Text + "' WHERE id=" + ds.Tables[0].Rows[listBox1.SelectedIndex].Field<long>("id");
+                command.Connection = m_dbConnection;
+                command.ExecuteNonQuery();
+                click1 = true;
+                refresh();
+                
+                MessageBox.Show("Сохранено");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error" + ex);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            textDirty = true;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            textDirty = true;
         }
     }
 }
